017668c6d68cfe74f94c35e64dda8a92abebc3a3
Christopher4113 <ch.lam1328@gmail.com>
2024-12-15 18:31:02 -0500

    some new stuff

--- code/src/service/AIModal.jsx
+++ code/src/service/AIModal.jsx
@@ -4,7 +4,7 @@ const {
     HarmBlockThreshold,
   } = require("@google/generative-ai");
   
-  const apiKey = process.env.GEMINI_API_KEY;
+  const apiKey = import.meta.env.VITE_GOOGLE_GEMINI_AI_API_KEY;
   const genAI = new GoogleGenerativeAI(apiKey);
   
   const model = genAI.getGenerativeModel({
@@ -16,11 +16,11 @@ const {
     topP: 0.95,
     topK: 40,
     maxOutputTokens: 8192,
-    responseMimeType: "text/plain",
+    responseMimeType: "application/json",
   };
   
-  async function run() {
-    const chatSession = model.startChat({
+  
+  export const chatSession = model.startChat({
       generationConfig,
       history: [
       ],
@@ -28,6 +28,5 @@ const {
   
     const result = await chatSession.sendMessage("INSERT_INPUT_HERE");
     console.log(result.response.text());
-  }
+
   
-  run();
